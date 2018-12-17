3359a3f5dbfb6e66a5aa0231bbfd5b2beac7f152
tmshenfs <xfshenfangsheng@126.com>
2018-12-17 17:58:37 +0800

    dockerpull.sh

---
+++ dockerpull.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+images=(kube-apiserver-v1.13.0 kube-controller-manager-v1.13.0 kube-scheduler-v1.13.0 kube-proxy-v1.13.0 pause-3.1 etcd-3.2.24 coredns-1.2.6)
+newimages=(kube-apiserver:v1.13.0 kube-controller-manager:v1.13.0 kube-scheduler:v1.13.0 kube-proxy:v1.13.0 pause:3.1 etcd:3.2.24 coredns:1.2.6)
+for image in ${images[@]} ; do
+  docker pull tmshenfs/kubernetes:$images
+  docker tag tmshenfs/kubernetes:$images gcr.io/google_containers/$newimages
+  docker rmi tmshenfs/kubernetes:$images
+done
